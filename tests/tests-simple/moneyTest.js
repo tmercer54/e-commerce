caf1240c5babea9ba9b2646e3f78e77f39467cc3
mercer.tyler1@gmail.com <mercer.tyler1@gmail.com>
2024-09-04 19:13:26 -0500

    created e-commerce platform where users are able to search, select, purchase, and track products using html, css, javascript, node.js

---
+++ tests/tests-simple/moneyTest.js
@@ -0,0 +1,29 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+console.log('test suite: formatCurrency')
+
+console.log('converts cents into dollars');
+
+if (formatCurrency(2095) === '20.95') {
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+
+console.log('work with 0');
+
+if (formatCurrency(0) === '0.00') {
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+
+console.log('round up to the nearest cent');
+
+if(formatCurrency(2000.5) === '20.01') {
+    console.log('passed');
+}else{
+    console.log('failed')
+}
